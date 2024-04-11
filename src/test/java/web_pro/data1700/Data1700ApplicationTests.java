0545f02fd85632a86ef19e16cad3784e03ef5c19
LEO <141844230+M-LEO-H@users.noreply.github.com>
2024-04-11 11:07:15 +0200

    Inital

---
+++ src/test/java/web_pro/data1700/Data1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package web_pro.data1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
